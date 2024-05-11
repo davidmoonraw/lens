388dc84637c90cb7b4e3e5373a566dbea8df0958
David Rojas Peck - Moonraw <david.mcmlxxxviii@gmail.com>
2024-05-11 09:41:22 -0600

    Add jest test for checking port environment variable

---
+++ test/server.test.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv').config({ path: '../.env' });
+const { EXPRESS_PORT } = process.env;
+
+describe('Environment variables', () => {
+    describe('Express connection port', () => {
+        test('Port is a defined number in environment variables', () => {
+            const isNumber = typeof(parseInt(EXPRESS_PORT)) === 'number';
+            expect(isNumber).toBeTruthy();
+        });
+    });
+});
