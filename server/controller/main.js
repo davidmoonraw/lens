fb3514877a46b14bfe38d55414a8f66f43c6a66f
David Rojas Peck - Moonraw <david.mcmlxxxviii@gmail.com>
2024-05-13 17:32:29 -0600

    Add form route

--- server/controller/main.js
+++ server/controller/main.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const Form = require('../form');
+
+
+const urlEncodedParser = bodyparser.urlencoded({extended:false})
 
 router.use((req,res,next)=>{
     console.log("Time:",Date.now());
@@ -8,5 +12,14 @@ router.use((req,res,next)=>{
 });
 
 router.use(express.static(path.join(__dirname, '..', '..', 'client')));
+
+
+router.post('/form', (req,res)=>{
+    const {email, text} = req.body;
+    const form =  new Form({email:email, text:text});
+    form.save().then(()=>console.log('New message!'));
+    res.json('done...');
+});
+
 
 module.exports = router;
