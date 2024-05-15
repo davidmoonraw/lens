c2fdf4d0ba96e38b7ad5e966ad7400a0165980eb
David Rojas Peck - Moonraw <david.mcmlxxxviii@gmail.com>
2024-05-15 15:50:26 -0600

    Rename variables based on logic

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
 require('dotenv').config({path:'.env'});
 require('./db');
 const express = require('express');
-const controller = require('./controller/main');
+const middleware = require('./handler/mount');
 
 const {EXPRESS_PORT} = process.env;
 
-
 const app = express();
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(controller);
 
+app.use(middleware);
 
 app.listen(EXPRESS_PORT, ()=>{
     console.log(`Server is running on port ${EXPRESS_PORT}`);
 });
-
