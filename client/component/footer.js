a9a881fa2332e3d0dfd2af735ac84e19f357e313
David Rojas Peck - Moonraw <david.mcmlxxxviii@gmail.com>
2024-05-17 11:31:44 -0600

    Refactor skeleton parts as js components

---
+++ client/component/footer.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+
+    let body  = document.querySelector('#main-section');
+    body.insertAdjacentHTML('beforeend', footerHTML)
+
+});
+
+
+const footerHTML = ` <!--Footer-->     
+<div class="row gy-5">
+<div class="col-12 text-center">
+<footer>Copyright Lens ~ 2024</footer>
+</div>
+</div>`
